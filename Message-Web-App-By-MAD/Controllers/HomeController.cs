e12971e7acc7cd5192668bfafab9803b8c479bae
aliDemiroz <dmrzmehmetali@gmail.com>
2024-05-08 12:03:08 +0300

    Beginning

---
+++ Message-Web-App-By-MAD/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Message_Web_App_By_MAD.Models;
+
+namespace Message_Web_App_By_MAD.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ Message-Web-App-By-MAD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Message_Web_App_By_MAD.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
